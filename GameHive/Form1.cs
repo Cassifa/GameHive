9925cbe70770e547578c80a30fdec9d078388f68
Casssifa(loacl) <3300595140@qq.com>
2024-11-25 19:56:40 +0800

    初始化基于WinForm的图形化界面程序，完成界面设计

---
+++ GameHive/Form1.cs
@@ -0,0 +1,43 @@
+namespace GameHive {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void rightPanel_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void LogListBox_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
