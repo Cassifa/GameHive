40f232f4d92c96b0f79296c2a2e3566c343d018a
Casssifa(loacl) <3300595140@qq.com>
2024-12-06 15:37:34 +0800

    搭建博弈树搜索框架

--- GameHive/Model/AIFactory/AbstractAIProduct/MinMax.cs
+++ GameHive/Model/AIFactory/AbstractAIProduct/MinMax.cs
@@ -9,9 +9,31 @@ using GameHive.Constants.RoleTypeEnum;
 
 namespace GameHive.Model.AIFactory.AbstractAIProduct {
     internal abstract class MinMax : AbstractAIStrategy {
-        int maxDeep;
-        public abstract bool IsHeWin(Role role);
-        public abstract bool IsEnd();
+        protected int maxDeep;
+        private Tuple<int, int> FinalDecide;
+        //决定Ai的风格偏好,加起来权重为10
+        //protected int DefendBias = 4, AttackBias = 6;
+        //获取可下棋点位
+        protected abstract List<Tuple<int, int>> GetAvailableMoves(List<List<Role>> board);
+        protected abstract int EvalNowSituation(List<List<Role>> currentBoard, Role role);
+        ////获取在X,Y落子带来的总收益(带风险偏好)
+        //protected abstract int evalXYTRoundScore(List<List<Role>> currentBoard, int x,int y, Role role);
+        ////计算 role 视角下在X,Y落子获得的攻击分数
+        //protected abstract int calculateXY(List<List<Role>> currentBoard, int x,int y, Role role);
 
+        //获取下一步移动
+        public override Tuple<int, int> GetNextAIMove(List<List<Role>> currentBoard) {
+            //计算最优值
+            EvalToGo(currentBoard, 1, int.MinValue, int.MaxValue);
+            return FinalDecide;
+        }
+        //执行博弈树搜索
+        private int EvalToGo(List<List<Role>> currentBoard, int depth, int alpha, int bata) {
+
+        }
+        //计算一条的估值
+        private int CalculateLine() {
+
+        }
     }
 }
